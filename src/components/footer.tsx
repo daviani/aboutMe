755f2183aa1a711350b7fcea879fdd7a55116fa0
Daviani <contact@daviani.dev>
2021-11-22 16:23:39 +0100

    Add improves responsiv design footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     return (
         <footer className='w-full py-5 px-4 '>
             <div className='flex flex-col md:flex-row items-center justify-between'>
-                <div className='inline-flex items-center '>
+                <div className='inline-flex items-center'>
                     <div className='w-3 h-3 p-2 mr-2 rounded-full bg-gradient-to-tr from-blue-100 to-blue-700' />
                     <h2
                         className='block p-2 text-xl font-medium tracking-tighter text-blue-700 dark:text-blue-200 lg:text-x lg:mr-8'
@@ -27,7 +27,7 @@ const Footer = () => {
                     {'  '}
                     {t("footer_main-subtitle")}
                 </div>
-                <div className='md:footer_box'>
+                <div className='footer_box'>
                     <span className='inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start'
                     >
                         <a href={t("footer_git-url")}
